4d198bd6db468dc48605b9281d13a32c35736e1b
waitme8888 <18601144707@163.com>
2016-12-23 14:43:47 +0800

    none

--- src/main/java/com/waitme/mybatis/dao/UserMapper.java
+++ src/main/java/com/waitme/mybatis/dao/UserMapper.java
@@ -1,7 +1,10 @@
 package com.waitme.mybatis.dao;
 
 import com.waitme.mybatis.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper {
 	
 	public User selectUserById(Long id);
